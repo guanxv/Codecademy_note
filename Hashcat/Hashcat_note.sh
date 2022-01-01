4c15426dc2707f29cab924d873740c9618be7dc7
guanxv <guanxv@gmail.com>
2022-01-02 09:56:08 +1100

    20220102

--- Hashcat/Hashcat_note.sh
+++ Hashcat/Hashcat_note.sh
@@ -53,6 +53,31 @@ hashcat -a 3 -m 0 example0.hash ?a?a?a?a?a?a #will try all the 6 keyspaces (uper
 
 #sample command using mask to crack shadow
 hashcat -a3 -m500 -o result.txt shadow ./Corporate_Masks-master/corp_8.hcmask
+'''
+Session..........: hashcat
+Status...........: Running
+Hash.Type........: md5crypt, MD5 (Unix), Cisco-IOS $1$ (MD5)
+Hash.Target......: $1$60a6wQfe$XQEdtkH8HMvQis1IZIwT50
+Time.Started.....: Sun Jan  2 09:54:51 2022 (40 secs)
+Time.Estimated...: Sun Jan  2 09:58:05 2022 (2 mins, 34 secs)
+Guess.Mask.......: ?l?u?d?d?d?d?d?d [8]
+Guess.Queue......: 41/21990 (0.19%)
+Speed.#1.........:  3460.6 kH/s (199.42ms) @ Accel:1024 Loops:1000 Thr:32 Vec:1
+Recovered........: 0/1 (0.00%) Digests, 0/1 (0.00%) Salts
+Progress.........: 139853824/676000000 (20.69%)
+Rejected.........: 0/139853824 (0.00%)
+Restore.Point....: 5046272/26000000 (19.41%)
+Restore.Sub.#1...: Salt:0 Amplifier:12-13 Iteration:0-1000
+Candidates.#1....: hS988577 -> hI993107
+Hardware.Mon.#1..: Temp: 75c Fan: 80% Util:100% Core:1860MHz Mem:6801MHz Bus:16
+
+'''
+
+
+hashcat -a0 -m500 -o result.txt shadow rockyou.txt -r ./rules/best64.rule --loopback
+
+hashcat -a0 -m500 -o result.txt shadow realuniq.lst -r ./rules/best64.rule --loopback
+
 
 
 # speed, with GTX 1660 , can crack 8 dig, digtial in 20 sec.
